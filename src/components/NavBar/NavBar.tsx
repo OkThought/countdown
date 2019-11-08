b104bddd03b430bbf3e12c54d2b61eccd73c5f5a
OkThought <bogush.vano@gmail.com>
2019-11-08 17:50:26 +0700

    Display nav items in a responsive drawer

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,26 +1,50 @@
-import React, {useMemo} from 'react'
-import {Toolbar, Typography, AppBar, makeStyles, Theme, Button} from '@material-ui/core'
+import React, {useMemo, Key, useState} from 'react'
+import {
+  Toolbar,
+  AppBar,
+  makeStyles,
+  Theme,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+  Hidden,
+  Drawer,
+  Typography,
+} from '@material-ui/core'
+import {Add as AddIcon, Menu as MenuIcon} from '@material-ui/icons'
 import {countdownPath, MINUTE} from '../../utils'
 import {AppBarProps} from '@material-ui/core/AppBar'
 import {Link} from 'react-router-dom'
 import routePatterns from '../../routePatterns'
+import {ListItemIconProps} from '@material-ui/core/ListItemIcon'
 
 //noinspection TypeScriptValidateTypes
 const useStyles = makeStyles((theme: Theme) => ({
   appBar: {},
-  link: {
+  toolbar: {
+    ...theme.mixins.toolbar,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navItem: {
     color: 'inherit',
     margin: theme.spacing(0.5),
   },
 }))
 
 type NavItem = {
+  key?: Key
   label: string
+  icon: ListItemIconProps['children']
   getUrl: () => string
 }
 
 export interface NavBarProps extends AppBarProps {
-  
+
 }
 
 function NavBar(props: NavBarProps) {
@@ -28,27 +52,37 @@ function NavBar(props: NavBarProps) {
     ...restProps
   } = props
 
-  const links = useMemo<NavItem[]>(() => [{
+  const [drawerOpen, drawerOpenSet] = useState(false)
+
+  const handleDrawerToggle = () => {
+    drawerOpenSet(!drawerOpen)
+  }
+
+  const items = useMemo<NavItem[]>(() => [{
     label: 'New',
+    icon: <AddIcon/>,
     getUrl: () => routePatterns.newCountdown,
   }, {
     label: 'New Year',
+    icon: <span role='img' aria-label='new year'>🎄</span>,
     getUrl: () => countdownPath({
       to: new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0, 0),
       title: 'New Year 🎄',
     }),
   }, {
     label: 'Midnight',
+    icon: <span role='img' aria-label='midnight'>🌙</span>,
     getUrl: () => {
       const tomorrow = new Date()
       tomorrow.setDate(tomorrow.getDate() + 1)
       return countdownPath({
         to: new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate(), 0, 0, 0, 0),
-        title: 'Midnight 🌃',
+        title: 'Midnight 🌙',
       })
     },
   }, {
     label: '5 minutes',
+    icon: <span role='img' aria-label='5 minutes'>5️⃣</span>,
     getUrl: () => countdownPath({
       from: 5 * MINUTE,
       title: '5 minutes',
@@ -56,23 +90,62 @@ function NavBar(props: NavBarProps) {
   }], [])
 
   const classes = useStyles(props)
-  
-  return (
-    <AppBar className={classes.appBar} {...restProps}>
-      <Toolbar>
-        {links.map(({label, getUrl}) =>
-          <Typography variant='h6' color='initial' key={label}>
-            <Button
-              className={classes.link}
-              component={Link}
-              to={getUrl}
-            >
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar}>
+        <Typography variant='h6'>
+          Countdown
+        </Typography>
+      </div>
+      <Divider/>
+      <List>
+        {items.map(({label, key = label, icon, getUrl}) =>
+          <ListItem button key={key} component={Link} to={getUrl}>
+            <ListItemIcon>
+              {icon}
+            </ListItemIcon>
+            <ListItemText>
               {label}
-            </Button>
-          </Typography>,
+            </ListItemText>
+          </ListItem>,
         )}
-      </Toolbar>
-    </AppBar>
+      </List>
+    </div>
+  )
+
+  return (
+    <>
+      <AppBar className={classes.appBar} {...restProps}>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            edge='start'
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon/>
+          </IconButton>
+        </Toolbar>
+        <nav>
+          <Hidden smUp implementation='css'>
+            <Drawer
+              open={drawerOpen}
+              variant='temporary'
+              anchor='left'
+              onClose={handleDrawerToggle}
+              ModalProps={{keepMounted: true}}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation='css'>
+            <Drawer open variant='permanent'>
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+      </AppBar>
+    </>
   )
 }
 
