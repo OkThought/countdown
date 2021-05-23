5c0a1fba893ecdda41e6434d8ed3825b92a692e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-23 10:58:44 +0000

    Deploy OkThought/countdown to github.com/OkThought/countdown.git:gh-pages

---
+++ precache-manifest.f8b1a365d24e737af82a9f34b6b55007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5aedaaec4beb1579e35b7a968d08b0",
+    "url": "/countdown/index.html"
+  },
+  {
+    "revision": "3afcde2fad50a1f7c635",
+    "url": "/countdown/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1bb5a27ede1b3fdae635",
+    "url": "/countdown/static/js/2.0f36f71f.chunk.js"
+  },
+  {
+    "revision": "3afcde2fad50a1f7c635",
+    "url": "/countdown/static/js/main.e4aa0a05.chunk.js"
+  },
+  {
+    "revision": "c67b54f5e8eaeb298317",
+    "url": "/countdown/static/js/runtime-main.27dd7b69.js"
+  }
+]);
